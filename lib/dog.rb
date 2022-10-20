976f6c4e1c4c7beed49d1b9388fbee1b70b1736f
Halkano <kudezx@gmail.com>
2022-10-20 15:00:53 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,32 @@
 class Dog
 
+    #setter_method
+    def name=(name)
+        @name = name
+    end
+
+    #getter_method
+    def name
+        @name
+    end
+
+        #breed_setter_method
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #breed_getter_method
+
+    def breed
+        @breed
+    end
+
 end
+
+dog1 = Dog.new
+dog1.name = "Mastif"
+
+puts dog1.name
+
+dog1.breed = "Bull Dog"
+puts dog1.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,33 @@
 class Person
 
+    #name_getter_method
+    def name=(name)
+        @name = name
+    end
+
+    #name_setter_method
+
+    def name
+        @name
+    end
+
+
+    #job_getter_method
+    def job=(job)
+        @job = job
+    end
+
+    #job_setter_method
+    def job
+        @job
+    end
+
 end
+
+mtu = Person.new
+mtu.name ="kudez"
+puts mtu.name
+
+
+mtu.job = "Doctor"
+puts mtu.job
